480b3316c4f05186b908c6008b2d9d48ee81c43f
cluzt <dien.duano@gmail.com>
2016-12-23 15:50:50 +0700

    update hello message

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -22,7 +22,7 @@ public class HelloController {
 
 	@RequestMapping("/hello/{name}")
 	public @ResponseBody String hello(@PathVariable String name) {
-		return "Hi (git update), " + name;
+		return "Hola, " + name;
 	}
 
 }
